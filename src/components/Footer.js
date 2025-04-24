f9030b93a1e00a4dc31dcfb150e7354eb96e4cc5
Arkadia-Spiral-Grid <officialarkadiacollective@gmail.com>
2025-04-24 03:33:35 +0100

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 Arkadia. All rights reserved.</p>
+      <div className="social-links">
+        <a href="#">Facebook</a>
+        <a href="#">Twitter</a>
+        <a href="#">Instagram</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
