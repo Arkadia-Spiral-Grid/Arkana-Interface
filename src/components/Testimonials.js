1da7791ee30b8713357aee783bd7fafdfbac57f4
Arkadia-Spiral-Grid <officialarkadiacollective@gmail.com>
2025-04-24 03:34:10 +0100

    Create Testimonials.js

---
+++ src/components/Testimonials.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Testimonials() {
+  return (
+    <section id="testimonials">
+      <h2>What Our Community Says</h2>
+      <div className="testimonial">
+        <p>"Arkadia has changed my life in ways I can't explain. I'm eternally grateful!"</p>
+        <cite>- Community Member</cite>
+      </div>
+    </section>
+  );
+}
+
+export default Testimonials;
